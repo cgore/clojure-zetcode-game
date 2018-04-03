9cf35435a22380b7ecabeca7948fc85359557e64
Christopher Mark Gore <cgore@cgore.com>
2018-04-02 21:43:08 -0500

    first basic bit to draw a frame

--- test/zetcode_game/core_test.clj
+++ test/zetcode_game/core_test.clj
@@ -1,5 +1,13 @@
 (ns zetcode-game.core-test
   (:require [clojure.test :refer :all]
-            [zetcode-game.core :refer :all]))
+            [zetcode-game.core :refer :all])
+  (:import (javax.swing JFrame)))
 
-
+(defn frame
+  []
+  (doto (JFrame.)
+    (.setSize 250 200)
+    (.setTitle "zetcode game")
+    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+    (.setLocationRelativeTo nil)
+    (.setVisible true)))
