3bf8b8bd394a5363da301b305dbdc7427546e92f
Christopher Mark Gore <cgore@cgore.com>
2018-04-02 22:00:01 -0500

    adding comments

--- test/zetcode_game/core_test.clj
+++ test/zetcode_game/core_test.clj
@@ -5,9 +5,9 @@
 
 (defn frame
   []
-  (doto (JFrame.)
-    (.setSize 250 200)
-    (.setTitle (str "zetcode game " (int (* 1000000 (rand)))))
-    (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
-    (.setLocationRelativeTo nil)
-    (.setVisible true)))
+  (doto (JFrame.) ; make a new JFrame
+    (.setSize 250 200) ; 250 wide, 200 tall
+    (.setTitle (str "zetcode game " (int (* 1000000 (rand))))) ; random title string
+    (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE) ; just dispose of the frame
+    (.setLocationRelativeTo nil) ; center the frame on the screen
+    (.setVisible true))) ; make it so
