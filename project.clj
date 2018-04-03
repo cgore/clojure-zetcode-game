bab1f59cd1eb02387aa1bcff20593235787a9b47
Christopher Mark Gore <cgore@cgore.com>
2018-04-02 21:23:13 -0500

    BSD-3-Clause license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zetcode-game "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "BSD-3-Clause"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
