65d81c4c2436f2e3a58db4e07a95194a84cce9f3
Christopher Mark Gore <cgore@cgore.com>
2018-04-13 16:01:05 -0500

    simple white frame

--- src/zetcode_game/core.clj
+++ src/zetcode_game/core.clj
@@ -1,2 +1,32 @@
-(ns zetcode-game.core)
+(ns zetcode-game.core
+  (:import (javax.swing JFrame
+                        JPanel)
+           (java.awt BasicStroke
+                     Color
+                     Dimension
+                     Graphics
+                     Graphics2D
+                     RenderingHints)
+           (java.awt.geom AffineTransform
+                          Ellipse2D)))
 
+
+(defn rendering-hints []
+  )
+
+(defn panel
+  []
+  (proxy [JPanel] []
+    (paintComponent [graphics]
+      (proxy-super paintComponent graphics)
+      )))
+
+(defn frame
+  []
+  (doto (JFrame.) ; make a new JFrame
+    (.setSize 250 200) ; 250 wide, 200 tall
+    (.setTitle (str "zetcode game " (int (* 1000000 (rand))))) ; random title string
+    (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE) ; just dispose of the frame
+    (.setLocationRelativeTo nil) ; center the frame on the screen
+    (.add (panel))
+    (.setVisible true))) ; make it so
--- test/zetcode_game/core_test.clj
+++ test/zetcode_game/core_test.clj
@@ -1,13 +1,3 @@
 (ns zetcode-game.core-test
   (:require [clojure.test :refer :all]
-            [zetcode-game.core :refer :all])
-  (:import (javax.swing JFrame)))
-
-(defn frame
-  []
-  (doto (JFrame.) ; make a new JFrame
-    (.setSize 250 200) ; 250 wide, 200 tall
-    (.setTitle (str "zetcode game " (int (* 1000000 (rand))))) ; random title string
-    (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE) ; just dispose of the frame
-    (.setLocationRelativeTo nil) ; center the frame on the screen
-    (.setVisible true))) ; make it so
+            [zetcode-game.core :refer :all]))
